71249dbf5eff391a9c50da4ed1a0b96e80a0a174
chrsfabian17 <chrsfabian17@gmail.com>
2016-09-26 08:55:16 -0600

    Tried GUI
    Linked buttons to view controller

--- SecondSwiftApp/ViewController.swift
+++ SecondSwiftApp/ViewController.swift
@@ -8,18 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var firstButton: UIButton!
+    
+    @IBOutlet weak var firstTextBox: UITextField!
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var clickedButton(sender: UIButton!)
+    
+    {
+    textLabel.text = "you clicked me!!"
+    textLabel.textColor = UIColor.cyanColor()
+    }
 
 }
 
